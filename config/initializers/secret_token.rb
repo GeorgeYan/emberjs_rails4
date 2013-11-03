940cecd92b21a9fec0851b45803fb396e81f0fc2
GeorgeYan <abcyys123@gmail.com>
2013-11-03 12:40:58 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberjsRails4::Application.config.secret_key_base = 'b9e2b0a7b5e4a596a0e8f3907389a0ab0feb6e09f6ff55405eae350d6440f1ebc9dac56a81eb02f963f75fd5a053752bc2bf17ffe4b6d56791f795306a045952'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberjsRails4::Application.config.session_store :cookie_store, key: '_emberjs_rails4_session'
